ed552a6961340711a08ac8b52d0c2ef06a248efe
MateusSantosF <mateusferreira.dev@gmail.com>
2022-09-29 14:37:51 -0300

    create scriptDatabase

---
+++ Database Scheme/scriptDatabase.sql
@@ -0,0 +1,27 @@
+CREATE TABLE `locacao_dvds`.`actor` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `surname` VARCHAR(45) NULL,
+  `premiereDate` DATE NULL,
+  PRIMARY KEY (`id`));
+  
+  CREATE TABLE `locacao_dvds`.`dvd` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NULL,
+  `releaseYear` VARCHAR(5) NULL,
+  `mainActorFK` INT NOT NULL,
+  `supportingActorFK` INT NOT NULL,
+  `duration` INT NULL,
+  `genderFK` INT NOT NULL,
+  `ageRatingFK` INT NOT NULL,
+  PRIMARY KEY (`id`));
+
+  CREATE TABLE `locacao_dvds`.`gender` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+  CREATE TABLE `locacao_dvds`.`agerating` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `description` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`));
